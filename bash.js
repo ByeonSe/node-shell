c93744f2cd558bb9e4f66502bc082456cb24b9ee
Seonae <seonaebyeon1@gmail.com>
2021-01-15 16:30:03 -0500

    final

--- bash.js
+++ bash.js
@@ -1,24 +1,3 @@
 process.stdout.write("prompt > ");
-
-process.stdin.on("data", (data) => {
-  
-
-    const cmd = data.toString().trim();
-  process.stdout.write("You typed: " + cmd);
-  process.stdout.write("\nprompt > ");
-
- 
-
-});
-
-process.stdin.on("data", (data) => {
-  
-
-  if (data.toString().trim() === "pwd") {
-    process.stdout.write(process.cwd());
-    process.stdout.write("\nprompt > ");
-  } 
-  
- 
-
-});
+const pwd = require("./pwd");
+const ls = require("./ls")
--- pwd.js
+++ pwd.js
@@ -1,18 +1,21 @@
 
+module.exports = pwd ();
+ 
+  function pwd () {
 
-
-process.stdin.on("data", (data) => {
+    process.stdin.on("data", (data) => {
   
-    if (data.toString().trim() === "pwd") {
-      process.stdout.write(process.cwd());
+      if (data.toString().trim() === "pwd") {
+        process.stdout.write(process.cwd());
+        process.stdout.write("\nprompt > ");
+      } 
+      else {
+        const cmd = data.toString().trim();
+      process.stdout.write("You typed: " + cmd);
       process.stdout.write("\nprompt > ");
-    } 
-    else {
-      const cmd = data.toString().trim();
-    process.stdout.write("You typed: " + cmd);
-    process.stdout.write("\nprompt > ");
-    }
-   
-  
-  });
+      }
+    
+    });
+
+  }
   
